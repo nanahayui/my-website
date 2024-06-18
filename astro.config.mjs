781fefbc43aee4415058346c95eee30bc7ecb679
nnyi7 <nlvyia7@gmail.com>
2024-06-19 07:59:59 +0900

    add: blog layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
-
 import mdx from '@astrojs/mdx'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx()],
+  integrations: [react(), tailwind(), mdx(), icon()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
