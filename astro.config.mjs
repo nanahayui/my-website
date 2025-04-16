987e5b629c6a7bce91f14c07d5446780551b7444
nnyi7 <nlvyia7@gmail.com>
2025-04-16 23:27:15 +0900

    fix: remove react error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import icon from 'astro-icon'
@@ -8,7 +7,7 @@ import remarkLinkCard from 'remark-link-card'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx(), icon()],
+  integrations: [tailwind(), mdx(), icon()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
