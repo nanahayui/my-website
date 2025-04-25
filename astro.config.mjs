a1eb1747ede4c8dd6b79294338e256c4f4ca56a8
nnyi7 <nlvyia7@gmail.com>
2025-04-25 16:42:36 +0900

    Cloudflare adapterを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import { defineConfig } from 'astro/config'
+import cloudflare from '@astrojs/cloudflare'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import icon from 'astro-icon'
 
 import remarkLinkCard from 'remark-link-card'
 
+import cloudflare from '@astrojs/cloudflare'
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), icon()],
+
   markdown: {
+    output: 'server',
+    adapter: cloudflare(),
     shikiConfig: {
       theme: 'github-dark',
       wrap: true,
@@ -22,4 +28,6 @@ export default defineConfig({
       ],
     ],
   },
+
+  adapter: cloudflare(),
 })
